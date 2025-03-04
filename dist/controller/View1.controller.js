962ad40ad285c96b1eaf1afcd7f2c61404a85e8e
Marco Martini <marco.martini@zi.uzh.ch>
2025-03-04 11:51:11 +0100

    added dist

---
+++ dist/controller/View1.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller","sheetjs/thirdparty/xlsx"],(e,r)=>{"use strict";return e.extend("sheetjs.controller.View1",{onInit(){console.log(r.version)}})});
+//# sourceMappingURL=View1.controller.js.map
